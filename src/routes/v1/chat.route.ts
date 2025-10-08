a0c9295ce49a9b68825d2c6ce64016d167fc4a92
pouria drd <pouriadrd@gmail.com>
2025-10-08 22:22:37 +0330

    added chat auth middleware and chat detail api

--- src/routes/v1/chat.route.ts
+++ src/routes/v1/chat.route.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import protect from "@/middlewares/auth.middleware";
 import asyncHandler from "@/middlewares/async-handler.middleware";
+import { chatAuthMiddleware } from "@/middlewares/chat.middleware";
 import { getChatMessages, getUserChats } from "@/controllers/chat.controller";
 
 const chatRouter = Router();
 
 chatRouter.get("/my-chats", protect, asyncHandler(getUserChats));
-chatRouter.get("/:chatId", protect, asyncHandler(getChatMessages));
+chatRouter.get("/:chatId", protect, chatAuthMiddleware, asyncHandler(getChatMessages));
 
 export default chatRouter;
