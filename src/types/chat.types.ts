0ba440c9a51ddcc63c2b53d4813de6d7fcd0fbf0
pouria drd <pouriadrd@gmail.com>
2025-10-13 00:35:11 +0330

    craeted chat model

---
+++ src/types/chat.types.ts
@@ -0,0 +1,17 @@
+import { Types } from "mongoose";
+import { BaseDocument } from "./common.types";
+
+export interface IChatDocument extends BaseDocument {
+    participants: Types.ObjectId[]; // exactly two users
+    lastMessage?: Types.ObjectId;
+}
+
+export interface Chat {
+    id: string;
+
+    participants: string[];
+    lastMessage?: string;
+
+    updatedAt: Date;
+    createdAt: Date;
+}
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,4 @@
+export * from "./chat.types";
 export * from "./user.types";
 export * from "./error.types";
 export * from "./common.types";
